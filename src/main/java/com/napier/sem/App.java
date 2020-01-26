0c5ab320f942d34630427e7be69ecf897dd425f5
c-grattan <56635397+c-grattan@users.noreply.github.com>
2020-01-26 20:37:52 +0000

    Update comments and make sample data more relevant

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -8,21 +8,26 @@ import org.bson.Document;
 public class App {
 
     public static void main(String[] args) {
-        // Connect to MongoDB
+        //Create interface with MongoDB
         MongoClient mongoClient = new MongoClient("mongo-dbserver");
-        // Get a database - will create when we use it
+
+        //Get a database
         MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
+
+        //Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
-        Document doc = new Document("name", "Kevin Chalmers")
-                .append("class", "Software Engineering Methods")
+
+        //Create test document
+        Document doc = new Document("name", "Connor Grattan")
+                .append("module", "Software Engineering Methods")
                 .append("year", "2018/19")
-                .append("result", new Document("CW", 95).append("EX", 85));
-        // Add document to collection
+                .append("result", new Document("CW", 95)
+                        .append("EX", 85));
+
+        //Add document to collection
         collection.insertOne(doc);
 
-        // Check document in collection
+        //Check that the document is in the collection
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
     }
