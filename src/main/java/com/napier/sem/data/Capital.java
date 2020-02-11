9f2c65e6247435edfc2723b24e8022a88623458c
refracc <40345422@live.napier.ac.uk>
2020-02-11 12:28:24 +0000

    Auto-generated push by refracc

--- src/main/java/com/napier/sem/data/Capital.java
+++ src/main/java/com/napier/sem/data/Capital.java
@@ -1,30 +1,46 @@
 package com.napier.sem.data;
 
 /**
- * <h1>Capital</h1>
- * <p>Stores the shit related to capitals</p>
+ * Stores the data regarding the capital cities of each country.
  *
  * @author Group 16
  * @version 0.1.0.2
  * @since 2020/10/02
  */
 public class Capital {
+
     private String name;
     private String country;
     private String population;
 
+    /**
+     * This method gets the name of the capital city.
+     * @return The name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * This method sets the name of a given capital city.
+     * @param name The name to be set.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * This method gets the name of the country the capital city belongs to.
+     * @return The country
+     */
     public String getCountry() {
         return country;
     }
 
+    /**
+     * This method sets the country the capital city belongs to.
+     * @param country The country to be set.
+     */
     public void setCountry(String country) {
         if (country.length() > 52) {
             System.out.println("Couldn't set country name: " + country + ", name set to: ''");
@@ -34,10 +50,18 @@ public class Capital {
         }
     }
 
+    /**
+     * This method grabs the population of the capital city.
+     * @return The population
+     */
     public String getPopulation() {
         return population;
     }
 
+    /**
+     * This method sets the population of a given capital city.
+     * @param population The population to be set.
+     */
     public void setPopulation(String population) {
         this.population = population;
     }
