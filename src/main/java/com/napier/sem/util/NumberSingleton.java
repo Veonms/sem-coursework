8ccde56a16a395a74f3c35b3ef32edea814e6c32
AnchoR-1 <40397870@live.napier.ac.uk>
2020-02-14 20:42:04 +0000

    added in comments to the singleton
    Signed-off-by: AnchoR-1 <40397870@live.napier.ac.uk>

--- src/main/java/com/napier/sem/util/NumberSingleton.java
+++ src/main/java/com/napier/sem/util/NumberSingleton.java
@@ -1,11 +1,27 @@
 package com.napier.sem.util;
 
+/***
+ * This will generate a number for each class individually (city, population, captial city and country)
+ * it does this by having a private constructor that can only be called from within the class
+ * and the only way it can be called is by using the instance variable
+ *
+ * @author Group 16
+ * @version 0.1.0.2
+ * @since 2020/14/02
+ */
 public class NumberSingleton {
 
-    private NumberSingleton() {}
+    private NumberSingleton() {
+    }
 
     private static NumberSingleton instance;
 
+    private int city = 0, country = 0, population = 0, capital = 0; // private ints for each one
+
+    /**
+     * the following if statement will check if the singelton exists and if it does another one can not be
+     * generated
+     */
     public static NumberSingleton getInstance() {
         if (instance == null) {
             instance = new NumberSingleton();
@@ -13,21 +29,19 @@ public class NumberSingleton {
         return instance;
     }
 
-    private int city = 0, country = 0, population = 0, capital = 0;
-
-    public int getCity() {
+    public int getCity() { // gets called for cities
         return city += 1;
     }
 
-    public int getCountry() {
+    public int getCountry() { // gets called for countries
         return country += 1;
     }
 
-    public int getPopulation() {
+    public int getPopulation() { // gets called for population
         return population += 1;
     }
 
-    public int getCapital() {
+    public int getCapital() { // gets called for captial cities
         return capital += 1;
     }
 }
