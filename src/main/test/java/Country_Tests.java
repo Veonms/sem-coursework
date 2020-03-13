df3816ac060057b19d45ce7dd5fbc007460e3d9d
c-grattan <56635397+c-grattan@users.noreply.github.com>
2020-03-13 19:31:02 +0000

    Test for get methods in Country added

--- src/main/test/java/Country_Tests.java
+++ src/main/test/java/Country_Tests.java
@@ -5,31 +5,31 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.napier.sem.data.Country;
 
 public class Country_Tests {
+    String name = "Test City";
+    String code = "TCi";
+    String continent = "Test Continent";
+    String region = "Test Region";
+    double surfaceArea = 12.34;
+    int population = 1234;
+    int independence = 0;
+    double lifeExpectancy = 34.12;
+    double grossNationalProduct = 10203040.25;
+    double oldGrossNationalProduct = 10203040.00;
+    String localName = "Mother Testland";
+    String government = "Chancellor Putin";
+    String stateHead = "Mr. Vlad";
+    int capital = 0;
+    String code2 = "iCT";
+
     @Test
     void toStringProper()
     {
         /*
         Tests that toString returns a string containing the correct data, properly formatted.
 
-        Tested by creating an expected string with set values, creating a city with the same values,
+        Tested by creating an expected string with set values, creating a Country with the same values,
         and comparing the result of toString against the expected value.
          */
-
-        String name = "Test City";
-        String code = "TCi";
-        String continent = "Test Continent";
-        String region = "Test Region";
-        double surfaceArea = 12.34;
-        int population = 1234;
-        int independence = 0;
-        double lifeExpectancy = 34.12;
-        double grossNationalProduct = 10203040.25;
-        double oldGrossNationalProduct = 10203040.00;
-        String localName = "Mother Testland";
-        String government = "Chancellor Putin";
-        String stateHead = "Mr. Vlad";
-        int capital = 0;
-        String code2 = "iCT";
 
         String expected = "[" +
                 "code='" + code + '\'' +
@@ -71,10 +71,370 @@ public class Country_Tests {
     /*
     Get Tests
 
-    All of these test that the get methods of City return the correct value.
-    Tests involve creating a City with the relevant attribute set to a fixed value and then
+    All of these test that the get methods of Country return the correct value.
+    Tests involve creating a Country with the relevant attribute set to a fixed value and then
     comparing that value with the result of the corresponding get method.
     */
+
+    @Test
+    void getCode()
+    {
+        String expected = code;
+
+        Country testCountry = new Country(expected,
+                name,
+                continent,
+                region,
+                surfaceArea,
+                independence,
+                population,
+                lifeExpectancy,
+                grossNationalProduct,
+                oldGrossNationalProduct,
+                localName,
+                government,
+                stateHead,
+                capital,
+                code2);
+
+        Assertions.assertEquals(expected, testCountry.getCode());
+    }
+
+    @Test
+    void getName()
+    {
+        String expected = name;
+
+        Country testCountry = new Country(code,
+                expected,
+                continent,
+                region,
+                surfaceArea,
+                independence,
+                population,
+                lifeExpectancy,
+                grossNationalProduct,
+                oldGrossNationalProduct,
+                localName,
+                government,
+                stateHead,
+                capital,
+                code2);
+
+        Assertions.assertEquals(expected, testCountry.getName());
+    }
+
+    @Test
+    void getContinent()
+    {
+        String expected = continent;
+
+        Country testCountry = new Country(code,
+                name,
+                expected,
+                region,
+                surfaceArea,
+                independence,
+                population,
+                lifeExpectancy,
+                grossNationalProduct,
+                oldGrossNationalProduct,
+                localName,
+                government,
+                stateHead,
+                capital,
+                code2);
+
+        Assertions.assertEquals(expected, testCountry.getContinent());
+    }
+
+    @Test
+    void getRegion()
+    {
+        String expected = region;
+
+        Country testCountry = new Country(code,
+                name,
+                continent,
+                expected,
+                surfaceArea,
+                independence,
+                population,
+                lifeExpectancy,
+                grossNationalProduct,
+                oldGrossNationalProduct,
+                localName,
+                government,
+                stateHead,
+                capital,
+                code2);
+
+        Assertions.assertEquals(expected, testCountry.getRegion());
+    }
+
+    @Test
+    void getSurfaceArea()
+    {
+        double expected = surfaceArea;
+
+        Country testCountry = new Country(code,
+                name,
+                continent,
+                region,
+                expected,
+                independence,
+                population,
+                lifeExpectancy,
+                grossNationalProduct,
+                oldGrossNationalProduct,
+                localName,
+                government,
+                stateHead,
+                capital,
+                code2);
+
+        Assertions.assertEquals(expected, testCountry.getSurfaceArea());
+    }
+
+    @Test
+    void getIndependence()
+    {
+        int expected = independence;
+
+        Country testCountry = new Country(code,
+                name,
+                continent,
+                region,
+                surfaceArea,
+                expected,
+                population,
+                lifeExpectancy,
+                grossNationalProduct,
+                oldGrossNationalProduct,
+                localName,
+                government,
+                stateHead,
+                capital,
+                code2);
+
+        Assertions.assertEquals(expected, testCountry.getIndependence());
+    }
+
+    @Test
+    void getPopulation()
+    {
+        int expected = population;
+
+        Country testCountry = new Country(code,
+                name,
+                continent,
+                region,
+                surfaceArea,
+                independence,
+                expected,
+                lifeExpectancy,
+                grossNationalProduct,
+                oldGrossNationalProduct,
+                localName,
+                government,
+                stateHead,
+                capital,
+                code2);
+
+        Assertions.assertEquals(expected, testCountry.getPopulation());
+    }
+
+    @Test
+    void getLifeExpectancy()
+    {
+        double expected = lifeExpectancy;
+
+        Country testCountry = new Country(code,
+                name,
+                continent,
+                region,
+                surfaceArea,
+                independence,
+                population,
+                expected,
+                grossNationalProduct,
+                oldGrossNationalProduct,
+                localName,
+                government,
+                stateHead,
+                capital,
+                code2);
+
+        Assertions.assertEquals(expected, testCountry.getLifeExpectancy());
+    }
+
+    @Test
+    void getGrossNationalProduct()
+    {
+        double expected = grossNationalProduct;
+
+        Country testCountry = new Country(code,
+                name,
+                continent,
+                region,
+                surfaceArea,
+                independence,
+                population,
+                lifeExpectancy,
+                expected,
+                oldGrossNationalProduct,
+                localName,
+                government,
+                stateHead,
+                capital,
+                code2);
+
+        Assertions.assertEquals(expected, testCountry.getGrossNationalProduct());
+    }
+
+    @Test
+    void getOldGrossNationalProduct()
+    {
+        double expected = oldGrossNationalProduct;
+
+        Country testCountry = new Country(code,
+                name,
+                continent,
+                region,
+                surfaceArea,
+                independence,
+                population,
+                lifeExpectancy,
+                grossNationalProduct,
+                expected,
+                localName,
+                government,
+                stateHead,
+                capital,
+                code2);
+
+        Assertions.assertEquals(expected, testCountry.getOldGrossNationalProduct());
+    }
+
+    @Test
+    void getLocalName()
+    {
+        String expected = localName;
+
+        Country testCountry = new Country(code,
+                name,
+                continent,
+                region,
+                surfaceArea,
+                independence,
+                population,
+                lifeExpectancy,
+                grossNationalProduct,
+                oldGrossNationalProduct,
+                expected,
+                government,
+                stateHead,
+                capital,
+                code2);
+
+        Assertions.assertEquals(expected, testCountry.getLocalName());
+    }
+
+    @Test
+    void getGovernment()
+    {
+        String expected = government;
+
+        Country testCountry = new Country(code,
+                name,
+                continent,
+                region,
+                surfaceArea,
+                independence,
+                population,
+                lifeExpectancy,
+                grossNationalProduct,
+                oldGrossNationalProduct,
+                localName,
+                expected,
+                stateHead,
+                capital,
+                code2);
+
+        Assertions.assertEquals(expected, testCountry.getGovernment());
+    }
+
+    @Test
+    void getStateHead()
+    {
+        String expected = stateHead;
+
+        Country testCountry = new Country(code,
+                name,
+                continent,
+                region,
+                surfaceArea,
+                independence,
+                population,
+                lifeExpectancy,
+                grossNationalProduct,
+                oldGrossNationalProduct,
+                localName,
+                government,
+                expected,
+                capital,
+                code2);
+
+        Assertions.assertEquals(expected, testCountry.getStateHead());
+    }
+
+    @Test
+    void getCapital()
+    {
+        int expected = capital;
+
+        Country testCountry = new Country(code,
+                name,
+                continent,
+                region,
+                surfaceArea,
+                independence,
+                population,
+                lifeExpectancy,
+                grossNationalProduct,
+                oldGrossNationalProduct,
+                localName,
+                government,
+                stateHead,
+                expected,
+                code2);
+
+        Assertions.assertEquals(expected, testCountry.getCapital());
+    }
+
+    @Test
+    void getCode2()
+    {
+        String expected = code2;
+
+        Country testCountry = new Country(code,
+                name,
+                continent,
+                region,
+                surfaceArea,
+                independence,
+                population,
+                lifeExpectancy,
+                grossNationalProduct,
+                oldGrossNationalProduct,
+                localName,
+                government,
+                stateHead,
+                capital,
+                expected);
+
+        Assertions.assertEquals(expected, testCountry.getCode2());
+    }
 
     /*
     Set Tests
